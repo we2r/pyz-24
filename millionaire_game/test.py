4d9d9c99524953337022ea1f1d400b855778a76a
ritaly <malgorzata.lyczywek@gmail.com>
2024-04-18 18:55:14 +0200

    add __eq__ example

--- millionaire_game/test.py
+++ millionaire_game/test.py
@@ -1,20 +1,37 @@
-class Example:
+class UpperExample():
+    pass
+
+class Example(UpperExample):
+    """This is example testing class"""
     def __init__(self, name):
         self.name = name
 
-    def __str__(self): # __repr__
-        return f'Jestem obiektem, a moje imie to: {self.name}'
+    # def __repr__(self): # __repr__
+    #     return f'Jestem obiektem, a moje imie to: {self.name}'
+
+    def __eq__(self, other):
+        if isinstance(other, Example):
+            return self.name == other.name
+        else:
+            return False
+
+
 
 
 obj_ex = Example('kubek')
+obj_ex2 = Example('kubek')
 print(obj_ex)
+print(obj_ex2)
+print(obj_ex == obj_ex2)
 
-print(obj_ex.__class__.__name__)
-if obj_ex.__class__.__name__ == 'Exmple':
-    print('Prawda!')
-
+# print(obj_ex)
+#
+# print(obj_ex.__class__.__name__)
+# if obj_ex.__class__.__name__ == 'Exmple':
+#     print('Prawda!')
 
 # number = 3.0
 # print(isinstance(number, float))
 
-print(isinstance(obj_ex, int))
+# print(isinstance(obj_ex, int))
+
