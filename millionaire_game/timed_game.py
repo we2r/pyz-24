4d9d0f57a2c8eafd61965f4ad6b1cf6d442a460e
ritaly <malgorzata.lyczywek@gmail.com>
2024-04-22 20:09:32 +0200

    add proprty

--- millionaire_game/timed_game.py
+++ millionaire_game/timed_game.py
@@ -16,3 +16,33 @@ class TimedGame(Game):
         else:
             print(f"Wrong! No more time left!")
             return False
+
+    @property
+    def best_time(self):
+        # ... mechanizm teorytyczny odmierzania czasu
+        return f'Player best time: {self.time_limit}'
+
+
+    def __str__(self):
+        return f'time--> {self.time_limit}'
+
+
+
+def main():
+    from question import Question
+    question_list = [
+        Question("What is the capital of France?", ["London", "Paris", "Berlin", "Madrid"], "Paris", difficulty='easy'),
+        Question("What is 2 + 2?", ["3", "4", "2", "5"], "4",  difficulty='easy'),
+        Question("Who wrote 'Macbeth'?", ["Shakespeare", "Austen", "Joyce", "Hemingway"], "Shakespeare", difficulty='easy'),
+    ]
+
+    game = TimedGame(question_list, 10)
+    print(game)
+    print(game.best_time)
+    game.time_limit = 100
+    print(game)
+    print(game.best_time)
+
+
+if __name__ == '__main__':
+    main()
